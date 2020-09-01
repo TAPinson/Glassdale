92733b8e8d53119f724b7f63ceb28238af184ad2
Travis Pinson <travispinson@gmail.com>
2020-09-01 08:49:23 -0500

    book2 ch3 assignment complete

---
+++ scripts/convictions/ConvictionProvider.js
@@ -0,0 +1,15 @@
+export let convictions = []
+
+export const useConvictions = () => {
+    return convictions.slice()
+}
+
+export const getConvictions = () => {
+    return fetch("https://criminals.glassdale.us/crimes")
+        .then(response => response.json())
+        .then(
+            parsedConviction => {
+                convictions = parsedConviction
+            }
+        )
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,16 @@
 import {OfficerList} from './officers/OffierList.js'
 import {CriminalList} from './criminals/CriminalList.js'
 
+
 OfficerList()
 CriminalList()
+
+
+// TEST FACILITY
+import { getConvictions } from './convictions/ConvictionProvider.js'
+import {ConvictionSelect} from './convictions/ConvictionSelect.js'
+
+
+getConvictions()
+.then(ConvictionSelect)
+//ConvictionSelect()
