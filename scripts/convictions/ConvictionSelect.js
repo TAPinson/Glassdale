4289d48e7083536eb63568938972078b0a5c2eee
Travis Pinson <travispinson@gmail.com>
2020-09-01 14:01:46 -0500

    ch2 book 3 complete - bug correction

--- scripts/convictions/ConvictionSelect.js
+++ scripts/convictions/ConvictionSelect.js
@@ -11,23 +11,24 @@ export const ConvictionSelect = () => {
     // Get all convictions from application state
     const convictions = useConvictions()
     console.log(convictions)
-    const render = convictionsCollection => {
-        /*
-            Use interpolation here to invoke the map() method on
-            the convictionsCollection to generate the option elements.
-            Look back at the example provided above.
-        */
-        contentTarget.innerHTML = `
-            <select class="dropdown" id="crimeSelect">
-                <option value="0">Please select a crime...</option>
-                ${
-                    convictionsCollection.map(crime => {
-                        return `<option value="${crime.name}>${crime.name}</option>`
-                    })
-                }
-            </select>
-        `
-    }
-    
     render(convictions)
+}
+
+const render = convictionsCollection => {
+   
+    /*
+        Use interpolation here to invoke the map() method on
+        the convictionsCollection to generate the option elements.
+        Look back at the example provided above.
+    */
+    contentTarget.innerHTML = `
+        <select class="dropdown" id="crimeSelect">
+            <option value="0">Please select a crime...</option>
+            ${
+                convictionsCollection.map(crime => {
+                    return `<option value="${crime.name}">${crime.name}</option>`
+                })
+            }
+        </select>
+    `
 }
