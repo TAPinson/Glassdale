74c4978efce94b5e9dab98c5aab5684c817e6a2a
Travis Pinson <travispinson@gmail.com>
2020-09-02 14:10:20 -0500

    corrected typo in variable names

--- scripts/criminals/Criminal.js
+++ scripts/criminals/Criminal.js
@@ -1,10 +1,10 @@
-export const CriminalHTML = (criminalOjb) => {
+export const CriminalHTML = (criminalObj) => {
     return `
-    <section id="criminals-${criminalOjb.id}" class="card-criminal">
-        <h3>Name: ${criminalOjb.name}</h3>
-        <div>Crime: ${criminalOjb.conviction}</div>
-        <div>Term Start: ${new Date(criminalOjb.incarceration.start).toLocaleDateString('en-US')}</div>
-        <div>Term End: ${new Date(criminalOjb.incarceration.end).toLocaleDateString('en-US')}</div>
+    <section id="criminals-${criminalObj.id}" class="card-criminal">
+        <h3>Name: ${criminalObj.name}</h3>
+        <div>Crime: ${criminalObj.conviction}</div>
+        <div>Term Start: ${new Date(criminalObj.incarceration.start).toLocaleDateString('en-US')}</div>
+        <div>Term End: ${new Date(criminalObj.incarceration.end).toLocaleDateString('en-US')}</div>
     </section>
     `
 }
