0e857a943d305b96f2520ec13f8b55b3e3ed1072
Travis Pinson <travispinson@gmail.com>
2020-09-03 13:47:11 -0500

    book2 ch2 complete

--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -9,25 +9,18 @@ const contentTarget = document.querySelector(".noteFormContainer")
 
 eventHub.addEventListener("click", clickEvent => {
     if(clickEvent.target.id === "saveNote"){
-
         const noteContent = document.querySelector("#noteForm--text")
         const noteCriminal = document.querySelector("#noteForm--criminal")
-
         if(noteCriminal.value !== "0"){
-
             const newNote = {
                 noteText: noteContent.value,
                 suspect: noteCriminal.value,
                 date: Date.now()
             }
-            
             saveNote(newNote);
         }else {
             window.alert("Choose a Suspect")
         }
-
-
-        
     }
 })
 
--- scripts/notes/NoteProvider.js
+++ scripts/notes/NoteProvider.js
@@ -3,15 +3,34 @@
 // get all the notes from database
 // add a note to the databate
 
-export const saveNote = noteObj => {
+
+const eventHub = document.querySelector(".container")
+let notes = []
+
+const dispatchStateChangeEvent = () => {
+    const noteStateChangedEvent = new CustomEvent("noteStateChanged")
+    eventHub.dispatchEvent(noteStateChangedEvent)
+}
+
+
+const getNotes = () => {
+    return fetch('http://localhost:8088/notes')
+        .then(response => response.json())
+        .then(parsedNotes => {
+            notes = parsedNotes
+        })
+
+}
+
+
+export const saveNote = note => {
     return fetch("http://localhost:8088/notes", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify(noteObj)
+        body: JSON.stringify(note)
     })
-    .then((result) => {
-        console.log("CELEBRATE RIGHT NOW")
-    })
+    .then(getNotes)
+    .then(dispatchStateChangeEvent)
 }
