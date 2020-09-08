fcd4fc41a5a678946680172904391d16c14706f3
Travis Pinson <travispinson@gmail.com>
2020-09-08 10:41:22 -0500

    hitting alibi button while info is populated now clears the data

--- scripts/criminals/AlibiDialog.js
+++ scripts/criminals/AlibiDialog.js
@@ -2,20 +2,26 @@ import {useCriminals} from './CriminalProvider.js'
 
 const eventHub = document.querySelector(".container")
 
+
 eventHub.addEventListener("associatesClicked", event => {
     //display all associates for criminals
     const targetCriminal = useCriminals().find(criminal => {
         return criminal.id === parseInt(event.detail.chosenCriminal)
     })
     const alibiTarget = document.querySelector(`.alibiDialog--${targetCriminal.id}`)
-    alibiTarget.innerHTML = `${
-        targetCriminal.known_associates.map(associate => {
-            return `
-            <h4>${associate.name}</h4>
-            <div>${associate.alibi}</div>
-            `
-        }).join("")
-    }`
+    const hTarget = document.querySelector("h4")
+    if (alibiTarget.contains(hTarget)) {
+        alibiTarget.innerHTML = ""
+    }else {
+        alibiTarget.innerHTML = `${
+            targetCriminal.known_associates.map(associate => {
+                return `
+                <h4>${associate.name}</h4>
+                <div>${associate.alibi}</div>
+                `
+            }).join("")
+        }`
+    }
 })
 
 
