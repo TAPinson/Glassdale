2e07aaa5f0f380ea777db94a4002fc1002a3b076
Travis Pinson <travispinson@gmail.com>
2020-09-17 15:53:19 -0500

    moved notes to render within criminal card instead of aside

--- scripts/notes/NoteList.js
+++ scripts/notes/NoteList.js
@@ -17,44 +17,34 @@ eventHub.addEventListener("click", event => {
     const isTargeted = event.target.classList.value
     if (isTargeted === "viewNoteButton") {
         const notes = useNotes()
-        const selectedCriminalID = event.target.id
+        const selectedCriminalID = parseInt(event.target.id)
         const criminals = useCriminals()
-
-
         //Find the criminal who matches on ID
         const selectedCriminal = criminals.find(criminal => {
             return selectedCriminalID == criminal.id
         })
-
-
         //Filter notes to only show those created by the matching criminal ID
         const matchingNotes = notes.filter(note => {
             return selectedCriminal.id === note.suspectID
         })
-        //console.log(matchingNotes[0].suspectID)
-        //console.log(selectedCriminal.id)
-
-        render(matchingNotes, criminals)
+        cardRender(matchingNotes, criminals)
     }
 })
 
 
-// const cardRender = (notes, suspects) => {
-//     const clearCheck = document.querySelector(".note--title")
-//     const contentTarget = document.querySelector(`#notesBox-${notes[0].suspectID}`)
-    
-//     if (contentTarget.contains(clearCheck)) {
-//         contentTarget.innerHTML = ""
-//     }else{
-        
-//         contentTarget.innerHTML = notes.map((noteObject) => {
-//             noteObject.suspectObj = suspects.find(suspect => {
-//                 return suspect.id === parseInt(noteObject.suspectID)
-//             })
-//                 return NoteHTMLConverter(noteObject)
-//             }).join("");
-//     }
-// }
+const cardRender = (notes, suspects) => {
+    const contentTarget = document.querySelector(`#notesBox-${notes[0].suspectID}`)
+    if (contentTarget.innerHTML !== "") {
+        contentTarget.innerHTML = ""
+    }else{
+        contentTarget.innerHTML = notes.map((noteObject) => {
+            noteObject.suspectObj = suspects.find(suspect => {
+                return suspect.id === parseInt(noteObject.suspectID)
+            })
+                return NoteHTMLConverter(noteObject)
+            }).join("");
+    }
+}
 
 
 // Render any provided notes to the dom in the viewer aside
